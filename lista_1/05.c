2810470a03c75aaf9cf74bcfe000077670c6f847
Caio Carvalho <caioldcarvalho@protonmail.com>
2024-10-26 11:19:19 -0300

    Add files via upload

---
+++ lista_1/05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  const int KM_POR_LITRO = 15;
+  float tempo_de_viagem, velocidade_media;
+
+  printf("Insira o tempo de viagem em horas e a velocidade média em Km/h: ");
+  scanf("%f %f", &tempo_de_viagem, &velocidade_media);
+
+  printf("O consumo de combustível foi: %.2fL.", (velocidade_media * tempo_de_viagem) / KM_POR_LITRO);
+
+  return 0;
+}
