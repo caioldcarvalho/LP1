b8ab5d980e84d666d4e1eaf14b776c989489850e
Caio Carvalho <caioldcarvalho@protonmail.com>
2024-10-26 12:05:13 -0300

    Add files via upload

---
+++ lista_1/10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int a, b;
+
+  printf("Insira dois valores a serem comparados: \n");
+  scanf("%d %d", &a, &b);
+
+  if ((a % b) == 0) {
+    printf("%d é divisível por %d\n", a, b);
+
+    return 0;
+  }
+
+    printf("%d não é divisível por %d\n", a, b);
+
+  return 0;
+}
