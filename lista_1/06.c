c094e974f6144962be355271de1f1991bab628d2
Caio Carvalho <caioldcarvalho@protonmail.com>
2024-11-06 21:45:26 -0300

    Update 06.c

--- lista_1/06.c
+++ lista_1/06.c
@@ -1,16 +1,15 @@
+//Ex06
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  const int boa_luminosidade = 15, watts_por_lampada = 60;
   float tamanho_comodo;
 
   printf("Insira o tamanho do cômodo: \n");
   scanf("%f", &tamanho_comodo);
 
-  float watts_necessarios = tamanho_comodo * boa_luminosidade;
-
-  printf("A quantidade de lâmpadas necessárias para iluminar este cômodo é %f\n", watts_necessarios / watts_por_lampada);
+  printf("A quantidade de lâmpadas necessárias para iluminar este cômodo é %f\n", ceil(tamanho_comodo, 4));
 
   return 0;
 }
